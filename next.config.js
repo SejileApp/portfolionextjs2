d6cf31315e973a960f9c744311c099ff24a7a15c
SejileApp <91794175+SejileApp@users.noreply.github.com>
2024-02-12 21:32:02 +0000

    change mongodb url

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
     env:{
       NEXTAUTH_URL:"https://my-portfolio-zyov.onrender.com/",
       NEXTAUTH_SECRET: "codingwithhicham",
-      DATABASE_MONGO_URL : "mongodb+srv://portfolio:portfolio123@cluster0.k8enfjh.mongodb.net//dbporfolio" ,
+      DATABASE_MONGO_URL : "mongodb+srv://portfolio:portfolio123@cluster0.k8enfjh.mongodb.net/dbporfolio" ,
       // DATABASE_MONGO_URL : "mongodb://127.0.0.1:27017/dbportfolio" ,
       TOKEN_SANITY : "skgPSIBw9KUesYkmnLcTxdSrG5xmY9V82i15PE6HGaSCNZDYzp79U7EgJ55U0Sk808bVeFpb29QJnpqH1hee54dfaa3Ahxljukn3Pa0Y8cLsKkwQZRMr64gIsfMlRVDfVrBCP8qT8hvxzKXjVxsxndJQMBIShrGP4kCSknd4JwtzvNIjR9If"
     }, 
