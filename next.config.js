9d2b5b169da90c21efa445b6e332aa36a2c29193
SejileApp <91794175+SejileApp@users.noreply.github.com>
2024-02-16 14:21:51 +0000

    delete env

--- next.config.js
+++ next.config.js
@@ -11,13 +11,7 @@ const nextConfig = {
       // unoptimized:true,
       },
       
-    env:{
-      NEXTAUTH_URL:"https://my-portfolio-zyov.onrender.com/",
-      NEXTAUTH_SECRET: "codingwithhicham",
-      DATABASE_MONGO_URL : "mongodb+srv://portfolio:portfolio123@cluster0.k8enfjh.mongodb.net/dbporfolio" ,
-      // DATABASE_MONGO_URL : "mongodb://127.0.0.1:27017/dbportfolio" ,
-      TOKEN_SANITY : "skgPSIBw9KUesYkmnLcTxdSrG5xmY9V82i15PE6HGaSCNZDYzp79U7EgJ55U0Sk808bVeFpb29QJnpqH1hee54dfaa3Ahxljukn3Pa0Y8cLsKkwQZRMr64gIsfMlRVDfVrBCP8qT8hvxzKXjVxsxndJQMBIShrGP4kCSknd4JwtzvNIjR9If"
-    }, 
+
 
     typescript: {
       // !! WARN !!
